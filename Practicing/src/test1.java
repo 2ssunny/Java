99c19310ee165787ddad8fcc99b2385973f4a1f1
2ssunny <eungseoni@gmail.com>
2022-04-13 10:09:17 +0900

    test

--- Practicing/src/test1.java
+++ Practicing/src/test1.java
@@ -1,5 +1,11 @@
+/**
+ * test1
+ */
 public class test1 {
+
     public static void main(String[] args) {
-        System.out.println("hello");
-    }
+        int a=10;
+        double b=5.5;
+        System.out.printf(a+b);
+    }  
 }
