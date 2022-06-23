f36aeeb40179b6ffdea3586efa0cf30591eac7ec
2ssunny <eungseoni@gmail.com>
2022-06-23 11:13:24 +0900

    abprint

---
+++ Practicing/src/abprint.java
@@ -0,0 +1,15 @@
+/**
+ * abprint
+ */
+public class abprint {
+
+    public static void main(String[] args) {
+        int a, b;
+        a=5;
+        b=15;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b);
+    }
+}
